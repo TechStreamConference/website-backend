7e63143a6d5d162f842825bd89972b9b0d5d2d12
Michael Gerhold <michael.gerhold@web.de>
2024-08-10 16:18:54 +0200

    add database migration for `User` table

---
+++ app/Database/Migrations/2024-08-10-143100_AddUser.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class AddUser extends Migration
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'time_created' => [
+                'type' => 'DATETIME',
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+                'null' => false,
+            ],
+            'time_updated' => [
+                'type' => 'DATETIME',
+                'null' => false,
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+                'on_update' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('User');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down()
+    {
+        $this->forge->dropTable('User');
+    }
+}
