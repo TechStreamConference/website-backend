169adef775c954ce8f1912825725c188e3e86abb
Michael Gerhold <michael.gerhold@web.de>
2024-10-13 18:10:16 +0200

    feat: convert number strings to numbers before including them in the response

--- app/Controllers/Event.php
+++ app/Controllers/Event.php
@@ -20,11 +20,11 @@ class Event extends BaseController
         }
         $eventModel = new EventModel();
         $event = $eventModel->getByYear($year);
+        $event['id'] = intval($event['id']);
+        $eventId = $event['id'];
         if ($event === null) {
             return $this->response->setStatusCode(404);
         }
-
-        $eventId = $event['id'];
 
         $sponsorModel = new SponsorModel();
         $sponsors = $sponsorModel->getPublished($eventId);
@@ -56,6 +56,24 @@ class Event extends BaseController
         }
 
         $event['year'] = $year;
+
+        foreach ($speakers as &$speaker) {
+            $speaker['id'] = intval($speaker['id']);
+            unset($speaker['user_id']);
+        }
+
+        foreach ($teamMembers as &$teamMember) {
+            $teamMember['id'] = intval($teamMember['id']);
+            unset($teamMember['user_id']);
+        }
+
+        foreach ($talks as &$talk) {
+            $talk['speaker_id'] = intval($talk['speaker_id']);
+            $talk['duration'] = intval($talk['duration']);
+            foreach ($talk['tags'] as &$tag) {
+                $tag['color_index'] = intval($tag['color_index']);
+            }
+        }
 
         return $this->response->setJSON([
             'event' => $event,
