b35b77376f50334b2b69e4841602ee510d01db61
Michael Gerhold <michael.gerhold@web.de>
2024-10-20 18:42:39 +0200

    style: improve formatting

--- app/Models/EventModel.php
+++ app/Models/EventModel.php
@@ -27,12 +27,18 @@ class EventModel extends Model
 
     public function get(int $eventId): array|null
     {
-        return $this->select('id, title, subtitle, start_date, end_date, discord_url, twitch_url, presskit_url, trailer_youtube_id, description_headline, description')->where('id', $eventId)->first();
+        return $this
+            ->select('id, title, subtitle, start_date, end_date, discord_url, twitch_url, presskit_url, trailer_youtube_id, description_headline, description')
+            ->where('id', $eventId)
+            ->first();
     }
 
     public function getByYear(int $year): array|null
     {
-        return $this->select('id, title, subtitle, start_date, end_date, discord_url, twitch_url, presskit_url, trailer_youtube_id, description_headline, description')->where('YEAR(start_date)', $year)->first();
+        return $this
+            ->select('id, title, subtitle, start_date, end_date, discord_url, twitch_url, presskit_url, trailer_youtube_id, description_headline, description')
+            ->where('YEAR(start_date)', $year)
+            ->first();
     }
 
     public function create(
