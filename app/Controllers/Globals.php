afbebbbd00e8dc60f1cbd45afc5699c427c29fb8
Michael Gerhold <michael.gerhold@web.de>
2024-10-20 18:43:50 +0200

    feat: include list of all years that have an event to the `globals` endpoint

--- app/Controllers/Globals.php
+++ app/Controllers/Globals.php
@@ -3,18 +3,31 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\EventModel;
 use App\Models\GlobalsModel;
 
 class Globals extends BaseController
 {
     public function get()
     {
+        // get global settings
         $globalsModel = model(GlobalsModel::class);
         $globals = $globalsModel->read();
         if ($globals === null) {
             // the global settings are faulty
             return $this->response->setStatusCode(500);
         }
+
+        // get all years with events
+        $eventModel = model(EventModel::class);
+        $events = $eventModel->getAll();
+        $yearsWithEvents = [];
+        foreach ($events as $event) {
+            $year = date('Y', strtotime($event['start_date']));
+            $yearsWithEvents[] = $year;
+        }
+        $globals['years_with_events'] = $yearsWithEvents;
+
         return $this->response->setJSON($globals);
     }
 }
