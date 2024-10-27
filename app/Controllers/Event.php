55de6d2125021a8f5e630ae13c001989305da13e
Michael Gerhold <michael.gerhold@web.de>
2024-10-27 11:43:18 +0100

    feat: rename `getICS()` to `getICalendarFile()`, move it to `Event` controller, adapt routes, add error check

--- app/Controllers/Event.php
+++ app/Controllers/Event.php
@@ -75,4 +75,51 @@ class Event extends BaseController
             'talks' => $talks,
         ]);
     }
+
+    public function getICalendarFile(int $year) {
+        // Start output buffering
+        ob_start();
+
+        // Get event id by year
+        $eventModel = new EventModel();
+        $event      = $eventModel->getByYear($year);
+        if ($event === null) {
+            // the requested year doesn't have an event
+            return $this->response->setStatusCode(404);
+        }
+
+        // Get all event talks
+        $talkModel = new TalkModel();
+        $talks     = $talkModel->getByEventId($event['id']);
+
+        // Generate ICS file content
+        $icsContent  = "BEGIN:VCALENDAR\n";
+        $icsContent .= "VERSION:2.0\n";
+        $icsContent .= "CALSCALE:GREGORIAN\n";
+
+        foreach ($talks as $talk) {
+            $icsContent .= "BEGIN:VEVENT\n";
+            $icsContent .= "SUMMARY:" . $talk['title'] . "\n";
+            $icsContent .= "DTSTART:" . date("Ymd\THis\Z", strtotime($talk['starts_at'])) . "\n";
+            $icsContent .= "DTEND:" . date("Ymd\THis\Z", strtotime($talk['starts_at']) + ($talk['duration'] * 60) ) . "\n";
+            $icsContent .= "LOCATION:" . $event['twitch_url'] . "\n";
+            $icsContent .= "DESCRIPTION:" . $talk['description'] . "\n";
+            $icsContent .= "END:VEVENT\n";
+        }
+
+        $icsContent .= "END:VCALENDAR\n";
+
+        // Clean output buffer and end buffering
+        ob_end_clean();
+
+        // Set filename
+        $filename = 'test-conf-' . $year . '.ics';
+
+        // Set headers and return ICS file
+        return $this->response
+            ->setHeader('Content-Type', 'text/calendar; charset=utf-8')
+            ->setHeader('Content-Disposition', 'attachment; filename="' . $filename . '"')
+            ->setHeader('Content-Length', strlen($icsContent))
+            ->setBody($icsContent);
+    }
 }
