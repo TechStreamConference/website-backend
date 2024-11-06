fa056f61a37bfa9705587e89be20d8cfb22f4419
Michael Gerhold <michael.gerhold@web.de>
2024-11-06 22:05:02 +0100

    fix: replace `assertJSON()` with `assertJSONExact()` and fix tests

--- tests/App/Controllers/AdminDashboardTest.php
+++ tests/App/Controllers/AdminDashboardTest.php
@@ -40,8 +40,11 @@ class AdminDashboardTest extends CIUnitTestCase
 
         $response = $this->get('/globals');
         $response->assertStatus(200);
-        $response->assertJSON([
+        $response->assertJSONExact([
             'footer_text' => 'This is the new text.',
+            'years_with_events' => [
+                2024,
+            ],
         ]);
     }
 
@@ -57,7 +60,7 @@ class AdminDashboardTest extends CIUnitTestCase
             ->withSession($sessionValues)
             ->get('/dashboard/admin/all-events');
         $response->assertStatus(200);
-        $response->assertJSON([
+        $response->assertJSONExact([
             [
                 'id' => 2,
                 'title' => 'Tech Stream Conference 2025',
@@ -70,6 +73,7 @@ class AdminDashboardTest extends CIUnitTestCase
                 'trailer_youtube_id' => 'IW1vQAB6B18',
                 'description_headline' => 'Komm\' ran!',
                 'description' => "In einer weit, weit entfernten Galaxis...\nDie Tech Stream Conference 2025 steht unter dem Motto \"Das Imperium schlägt zurück!\".\nSei dabei, wenn wir die dunkle Seite der Macht beleuchten und uns mit den dunklen Machenschaften der Technik beschäftigen. Freu dich auf unterhaltsame und interessante Vorträge – von der Community für die Community. Die Vortragenden stammen aus der Technik-Bubble von Twitch. Dazu gibt es noch \"special guests\" und Überraschungen.\nAlso sei gespannt!",
+                'schedule_visible_from' => '2025-06-22 12:00:00',
                 'publish_date' => null,
             ],
             [
@@ -84,6 +88,7 @@ class AdminDashboardTest extends CIUnitTestCase
                 'trailer_youtube_id' => 'IW1vQAB6B18',
                 'description_headline' => 'Sei dabei!',
                 'description' => "Spannende Vorträge aus den Bereichen Programmierung, Maker-Szene und Spieleentwicklung erwarten dich.\nWir möchten dich herzlich einladen, an unserer ersten Online-Konferenz teilzunehmen. Freu dich auf unterhaltsame und interessante Vorträge – von der Community für die Community. Die Vortragenden stammen aus der Technik-Bubble von Twitch. Dazu gibt es noch \"special guests\" und Überraschungen. Also sei gespannt!",
+                'schedule_visible_from' => '2024-06-22 12:00:00',
                 'publish_date' => '2024-01-01 12:00:00',
             ],
         ]);
@@ -101,20 +106,58 @@ class AdminDashboardTest extends CIUnitTestCase
             ->withSession($sessionValues)
             ->withBodyFormat('json')
             ->put('/dashboard/admin/event/1', [
-                "title" => "New Event Title",
-                "subtitle" => "New Event Subtitle",
-                "description" => "New Event Description",
-                "start_date" => "2025-11-05",
-                "end_date" => "2025-11-06",
-                "discord_url" => "https://discord.gg/123456",
-                "twitch_url" => "https://twitch.tv/123456",
-                "presskit_url" => "https://presskit.com/123456",
-                "trailer_youtube_id" => "123456",
-                "description_headline" => "New Event Description Headline",
-                "schedule_visible_from" => "2025-11-05 12:00:00",
-                "publish_date" => "2025-11-05 12:00:00",
+                'title' => 'New Event Title',
+                'subtitle' => 'New Event Subtitle',
+                'description' => 'New Event Description',
+                'start_date' => '2025-11-05',
+                'end_date' => '2025-11-06',
+                'discord_url' => 'https://discord.gg/123456',
+                'twitch_url' => 'https://twitch.tv/123456',
+                'presskit_url' => 'https://presskit.com/123456',
+                'trailer_youtube_id' => '123456',
+                'description_headline' => 'New Event Description Headline',
+                'schedule_visible_from' => '2025-11-05 12:00:00',
+                'publish_date' => '2025-11-05 12:00:00',
             ]);
         $response->assertStatus(204);
+
+        // additionally, we will check the updated values (to have a round-trip test)
+        $response = $this
+            ->withSession($sessionValues)
+            ->get('/dashboard/admin/all-events');
+        $response->assertStatus(200);
+        $response->assertJSONExact([
+            [
+                'id' => 1,
+                'title' => 'New Event Title',
+                'subtitle' => 'New Event Subtitle',
+                'start_date' => '2025-11-05',
+                'end_date' => '2025-11-06',
+                'discord_url' => 'https://discord.gg/123456',
+                'twitch_url' => 'https://twitch.tv/123456',
+                'presskit_url' => 'https://presskit.com/123456',
+                'trailer_youtube_id' => '123456',
+                'description_headline' => 'New Event Description Headline',
+                'description' => 'New Event Description',
+                'schedule_visible_from' => '2025-11-05 12:00:00',
+                'publish_date' => '2025-11-05 12:00:00',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Tech Stream Conference 2025',
+                'subtitle' => 'Das Imperium schlägt zurück!',
+                'start_date' => '2025-06-22',
+                'end_date' => '2025-06-23',
+                'discord_url' => 'https://discord.com/invite/tp4EnphfKb',
+                'twitch_url' => 'https://www.twitch.tv/coder2k',
+                'presskit_url' => 'https://test-conf.de/Test-Conf-Presskit.zip',
+                'trailer_youtube_id' => 'IW1vQAB6B18',
+                'description_headline' => 'Komm\' ran!',
+                'description' => "In einer weit, weit entfernten Galaxis...\nDie Tech Stream Conference 2025 steht unter dem Motto \"Das Imperium schlägt zurück!\".\nSei dabei, wenn wir die dunkle Seite der Macht beleuchten und uns mit den dunklen Machenschaften der Technik beschäftigen. Freu dich auf unterhaltsame und interessante Vorträge – von der Community für die Community. Die Vortragenden stammen aus der Technik-Bubble von Twitch. Dazu gibt es noch \"special guests\" und Überraschungen.\nAlso sei gespannt!",
+                'schedule_visible_from' => '2025-06-22 12:00:00',
+                'publish_date' => null,
+            ]
+        ]);
     }
 
     public function testUpdateEvent_ValidNullValues_Returns204()
@@ -126,20 +169,58 @@ class AdminDashboardTest extends CIUnitTestCase
             ->withSession($sessionValues)
             ->withBodyFormat('json')
             ->put('/dashboard/admin/event/1', [
-                "title" => "New Event Title",
-                "subtitle" => "New Event Subtitle",
-                "description" => "New Event Description",
-                "start_date" => "2025-11-05",
-                "end_date" => "2025-11-06",
-                "discord_url" => null,
-                "twitch_url" => null,
-                "presskit_url" => null,
-                "trailer_youtube_id" => null,
-                "description_headline" => "New Event Description Headline",
-                "schedule_visible_from" => null,
-                "publish_date" => null,
+                'title' => 'New Event Title',
+                'subtitle' => 'New Event Subtitle',
+                'description' => 'New Event Description',
+                'start_date' => '2025-11-05',
+                'end_date' => '2025-11-06',
+                'discord_url' => null,
+                'twitch_url' => null,
+                'presskit_url' => null,
+                'trailer_youtube_id' => null,
+                'description_headline' => 'New Event Description Headline',
+                'schedule_visible_from' => null,
+                'publish_date' => null,
             ]);
         $response->assertStatus(204);
+
+        // additionally, we will check the updated values (to have a round-trip test)
+        $response = $this
+            ->withSession($sessionValues)
+            ->get('/dashboard/admin/all-events');
+        $response->assertStatus(200);
+        $response->assertJSONExact([
+            [
+                'id' => 1,
+                'title' => 'New Event Title',
+                'subtitle' => 'New Event Subtitle',
+                'start_date' => '2025-11-05',
+                'end_date' => '2025-11-06',
+                'discord_url' => null,
+                'twitch_url' => null,
+                'presskit_url' => null,
+                'trailer_youtube_id' => null,
+                'description_headline' => 'New Event Description Headline',
+                'description' => 'New Event Description',
+                'schedule_visible_from' => null,
+                'publish_date' => null,
+            ],
+            [
+                'id' => 2,
+                'title' => 'Tech Stream Conference 2025',
+                'subtitle' => 'Das Imperium schlägt zurück!',
+                'start_date' => '2025-06-22',
+                'end_date' => '2025-06-23',
+                'discord_url' => 'https://discord.com/invite/tp4EnphfKb',
+                'twitch_url' => 'https://www.twitch.tv/coder2k',
+                'presskit_url' => 'https://test-conf.de/Test-Conf-Presskit.zip',
+                'trailer_youtube_id' => 'IW1vQAB6B18',
+                'description_headline' => 'Komm\' ran!',
+                'description' => "In einer weit, weit entfernten Galaxis...\nDie Tech Stream Conference 2025 steht unter dem Motto \"Das Imperium schlägt zurück!\".\nSei dabei, wenn wir die dunkle Seite der Macht beleuchten und uns mit den dunklen Machenschaften der Technik beschäftigen. Freu dich auf unterhaltsame und interessante Vorträge – von der Community für die Community. Die Vortragenden stammen aus der Technik-Bubble von Twitch. Dazu gibt es noch \"special guests\" und Überraschungen.\nAlso sei gespannt!",
+                'schedule_visible_from' => '2025-06-22 12:00:00',
+                'publish_date' => null,
+            ],
+        ]);
     }
 
     public function testUpdateEvent_RequiredValueIsNull_Returns400()
@@ -151,18 +232,18 @@ class AdminDashboardTest extends CIUnitTestCase
             ->withSession($sessionValues)
             ->withBodyFormat('json')
             ->put('/dashboard/admin/event/1', [
-                "title" => "New Event Title",
-                "subtitle" => "New Event Subtitle",
-                "description" => "New Event Description",
-                "start_date" => "2025-11-05",
-                "end_date" => "2025-11-06",
-                "discord_url" => null,
-                "twitch_url" => null,
-                "presskit_url" => null,
-                "trailer_youtube_id" => null,
-                "description_headline" => null, // must not be null
-                "schedule_visible_from" => null,
-                "publish_date" => null,
+                'title' => 'New Event Title',
+                'subtitle' => 'New Event Subtitle',
+                'description' => 'New Event Description',
+                'start_date' => '2025-11-05',
+                'end_date' => '2025-11-06',
+                'discord_url' => null,
+                'twitch_url' => null,
+                'presskit_url' => null,
+                'trailer_youtube_id' => null,
+                'description_headline' => null, // must not be null
+                'schedule_visible_from' => null,
+                'publish_date' => null,
             ]);
         $response->assertStatus(400);
     }
@@ -193,6 +274,59 @@ class AdminDashboardTest extends CIUnitTestCase
                 "publish_date" => "2025-11-05 12:00:00",
             ]);
         $response->assertStatus(201);
+
+        // additionally, we will check the created values (to have a round-trip test)
+        $response = $this
+            ->withSession($sessionValues)
+            ->get('/dashboard/admin/all-events');
+        $response->assertStatus(200);
+        $response->assertJSONExact([
+            [
+                'id' => 3,
+                'title' => 'New Event Title',
+                'subtitle' => 'New Event Subtitle',
+                'start_date' => '2025-11-05',
+                'end_date' => '2025-11-06',
+                'discord_url' => 'https://discord.gg/123456',
+                'twitch_url' => 'https://twitch.tv/123456',
+                'presskit_url' => 'https://presskit.com/123456',
+                'trailer_youtube_id' => '123456',
+                'description_headline' => 'New Event Description Headline',
+                'description' => 'New Event Description',
+                'schedule_visible_from' => '2025-11-05 12:00:00',
+                'publish_date' => '2025-11-05 12:00:00',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Tech Stream Conference 2025',
+                'subtitle' => 'Das Imperium schlägt zurück!',
+                'start_date' => '2025-06-22',
+                'end_date' => '2025-06-23',
+                'discord_url' => 'https://discord.com/invite/tp4EnphfKb',
+                'twitch_url' => 'https://www.twitch.tv/coder2k',
+                'presskit_url' => 'https://test-conf.de/Test-Conf-Presskit.zip',
+                'trailer_youtube_id' => 'IW1vQAB6B18',
+                'description_headline' => 'Komm\' ran!',
+                'description' => "In einer weit, weit entfernten Galaxis...\nDie Tech Stream Conference 2025 steht unter dem Motto \"Das Imperium schlägt zurück!\".\nSei dabei, wenn wir die dunkle Seite der Macht beleuchten und uns mit den dunklen Machenschaften der Technik beschäftigen. Freu dich auf unterhaltsame und interessante Vorträge – von der Community für die Community. Die Vortragenden stammen aus der Technik-Bubble von Twitch. Dazu gibt es noch \"special guests\" und Überraschungen.\nAlso sei gespannt!",
+                'schedule_visible_from' => '2025-06-22 12:00:00',
+                'publish_date' => null,
+            ],
+            [
+                "id" => 1,
+                "title" => "Tech Stream Conference 2024",
+                "subtitle" => "Spannende Vorträge aus den Bereichen Programmierung, Maker-Szene und Spieleentwicklung erwarten dich.",
+                "start_date" => "2024-06-22",
+                "end_date" => "2024-06-23",
+                "discord_url" => "https://discord.com/invite/tp4EnphfKb",
+                "twitch_url" => "https://www.twitch.tv/coder2k",
+                "presskit_url" => "https://test-conf.de/Test-Conf-Presskit.zip",
+                "trailer_youtube_id" => "IW1vQAB6B18",
+                "description_headline" => "Sei dabei!",
+                "description" => "Spannende Vorträge aus den Bereichen Programmierung, Maker-Szene und Spieleentwicklung erwarten dich.\nWir möchten dich herzlich einladen, an unserer ersten Online-Konferenz teilzunehmen. Freu dich auf unterhaltsame und interessante Vorträge – von der Community für die Community. Die Vortragenden stammen aus der Technik-Bubble von Twitch. Dazu gibt es noch \"special guests\" und Überraschungen. Also sei gespannt!",
+                "schedule_visible_from" => "2024-06-22 12:00:00",
+                "publish_date" => "2024-01-01 12:00:00",
+            ],
+        ]);
     }
 
     public function testCreateEvent_ValidNullValues_Returns201()
@@ -218,6 +352,59 @@ class AdminDashboardTest extends CIUnitTestCase
                 "publish_date" => null,
             ]);
         $response->assertStatus(201);
+
+        // additionally, we will check the created values (to have a round-trip test)
+        $response = $this
+            ->withSession($sessionValues)
+            ->get('/dashboard/admin/all-events');
+        $response->assertStatus(200);
+        $response->assertJSONExact([
+            [
+                'id' => 3,
+                'title' => 'New Event Title',
+                'subtitle' => 'New Event Subtitle',
+                'start_date' => '2025-11-05',
+                'end_date' => '2025-11-06',
+                'discord_url' => null,
+                'twitch_url' => null,
+                'presskit_url' => null,
+                'trailer_youtube_id' => null,
+                'description_headline' => 'New Event Description Headline',
+                'description' => 'New Event Description',
+                'schedule_visible_from' => null,
+                'publish_date' => null,
+            ],
+            [
+                'id' => 2,
+                'title' => 'Tech Stream Conference 2025',
+                'subtitle' => 'Das Imperium schlägt zurück!',
+                'start_date' => '2025-06-22',
+                'end_date' => '2025-06-23',
+                'discord_url' => 'https://discord.com/invite/tp4EnphfKb',
+                'twitch_url' => 'https://www.twitch.tv/coder2k',
+                'presskit_url' => 'https://test-conf.de/Test-Conf-Presskit.zip',
+                'trailer_youtube_id' => 'IW1vQAB6B18',
+                'description_headline' => 'Komm\' ran!',
+                'description' => "In einer weit, weit entfernten Galaxis...\nDie Tech Stream Conference 2025 steht unter dem Motto \"Das Imperium schlägt zurück!\".\nSei dabei, wenn wir die dunkle Seite der Macht beleuchten und uns mit den dunklen Machenschaften der Technik beschäftigen. Freu dich auf unterhaltsame und interessante Vorträge – von der Community für die Community. Die Vortragenden stammen aus der Technik-Bubble von Twitch. Dazu gibt es noch \"special guests\" und Überraschungen.\nAlso sei gespannt!",
+                'schedule_visible_from' => "2025-06-22 12:00:00",
+                'publish_date' => null,
+            ],
+            [
+                "id" => 1,
+                "title" => "Tech Stream Conference 2024",
+                "subtitle" => "Spannende Vorträge aus den Bereichen Programmierung, Maker-Szene und Spieleentwicklung erwarten dich.",
+                "start_date" => "2024-06-22",
+                "end_date" => "2024-06-23",
+                "discord_url" => "https://discord.com/invite/tp4EnphfKb",
+                "twitch_url" => "https://www.twitch.tv/coder2k",
+                "presskit_url" => "https://test-conf.de/Test-Conf-Presskit.zip",
+                "trailer_youtube_id" => "IW1vQAB6B18",
+                "description_headline" => "Sei dabei!",
+                "description" => "Spannende Vorträge aus den Bereichen Programmierung, Maker-Szene und Spieleentwicklung erwarten dich.\nWir möchten dich herzlich einladen, an unserer ersten Online-Konferenz teilzunehmen. Freu dich auf unterhaltsame und interessante Vorträge – von der Community für die Community. Die Vortragenden stammen aus der Technik-Bubble von Twitch. Dazu gibt es noch \"special guests\" und Überraschungen. Also sei gespannt!",
+                "schedule_visible_from" => "2024-06-22 12:00:00",
+                "publish_date" => "2024-01-01 12:00:00",
+            ],
+        ]);
     }
 
     public function testCreateEvent_RequiredValueIsNull_Returns400()
