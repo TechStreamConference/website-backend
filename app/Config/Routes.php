1c56b627122606eb10a5b8aa9ce9fd824ec2ccb3
Michael Gerhold <michael.gerhold@web.de>
2024-12-08 13:19:09 +0100

    feat: test sending mails

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -41,3 +41,5 @@ $routes->put('dashboard/admin/approval/team-member/(:num)/request-changes', [App
 $routes->get('dashboard/admin/approval/social-media-link', [Approval::class, 'getPendingSocialMediaLinks'], ['filter' => AdminAuthFilter::class]);
 $routes->put('dashboard/admin/approval/social-media-link/(:num)', [Approval::class, 'approveSocialMediaLink'], ['filter' => AdminAuthFilter::class]);
 $routes->put('dashboard/admin/approval/social-media-link/(:num)/request-changes', [Approval::class, 'requestChangesForSocialMediaLink'], ['filter' => AdminAuthFilter::class]);
+
+$routes->get('test', [Account::class, 'mailTest']);
