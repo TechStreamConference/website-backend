9306eb2c14988e3d3285bf6c0af6b6a761e0229e
Michael Gerhold <michael.gerhold@web.de>
2025-01-12 13:05:55 +0100

    feat: function to delete images

--- app/Controllers/Image.php
+++ app/Controllers/Image.php
@@ -4,10 +4,33 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\Files\File;
+use CodeIgniter\HTTP\ResponseInterface;
 
 class Image extends BaseController
 {
     public function get(string $filename)
+    {
+        $path = $this->getPath($filename);
+        if ($path instanceof ResponseInterface) {
+            return $path;
+        }
+        $file = new File($path);
+        $mimeType = $file->getMimeType();
+        $this->response->setHeader('Content-Type', $mimeType);
+        return $this->response->setBody(file_get_contents($path));
+    }
+
+    public function delete(string $filename)
+    {
+        $path = $this->getPath($filename);
+        if ($path instanceof ResponseInterface) {
+            return $path;
+        }
+        unlink($path);
+        return $this->response->setStatusCode(204);
+    }
+
+    private function getPath(string $filename): string|ResponseInterface
     {
         // prevent path traversal (even though CodeIgniter should already prevent this)
         $path = realpath(WRITEPATH . 'uploads' . DIRECTORY_SEPARATOR . $filename);
@@ -20,9 +43,6 @@ class Image extends BaseController
         if (!file_exists($path)) {
             return $this->response->setStatusCode(404);
         }
-        $file = new File($path);
-        $mimeType = $file->getMimeType();
-        $this->response->setHeader('Content-Type', $mimeType);
-        return $this->response->setBody(file_get_contents($path));
+        return $path;
     }
 }
