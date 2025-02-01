a50c9bf04e548318d9eb26cf35a3f29bbe9dd8ae
Michael Gerhold <michael.gerhold@web.de>
2025-02-01 13:01:18 +0100

    feat: include URL in social media link related emails

--- app/Views/email/admin/social_media_link_requested_changes.php
+++ app/Views/email/admin/social_media_link_requested_changes.php
@@ -1,6 +1,6 @@
 Liebe:r Tech Stream Conference Admin,
 
-<?= esc($admin) ?> hat gerade den Social-Media-Link von <?= esc($username) ?> abgelehnt und die folgenden Änderungen angefordert:
+<?= esc($admin) ?> hat gerade den Social-Media-Link „<?= esc($url) ?>” von <?= esc($username) ?> abgelehnt und die folgenden Änderungen angefordert:
 
 <?= esc($requestedChanges) ?>
 
