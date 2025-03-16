f395948c2fb8e279c9c5ad45b2648f2363aab1c3
Michael Gerhold <michael.gerhold@web.de>
2025-03-16 21:32:30 +0100

    fix: wrong quotes

--- app/Views/email/contributor/social_media_link_approved.php
+++ app/Views/email/contributor/social_media_link_approved.php
@@ -1,6 +1,6 @@
 Liebe:r <?= esc($username) ?>,
 
-dein Social-Media-Link „<?= esc($url) ?>” wurde freigeschaltet. Du kannst deine aktuellen Daten jederzeit auf unserer Website einsehen.
+dein Social-Media-Link „<?= esc($url) ?>“ wurde freigeschaltet. Du kannst deine aktuellen Daten jederzeit auf unserer Website einsehen.
 
 Viele Grüße,
 das Tech Stream Conference Team
