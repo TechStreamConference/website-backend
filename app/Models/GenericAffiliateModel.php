e61996528e0e3663a34157ae9bee24e8242d6b98
Michael Gerhold <michael.gerhold@web.de>
2025-06-21 15:45:03 +0200

    feat: add alternative logo fields for sponsors and media partners

--- app/Models/GenericAffiliateModel.php
+++ app/Models/GenericAffiliateModel.php
@@ -8,12 +8,12 @@ class GenericAffiliateModel extends Model
 {
     // child classes will have to set the protected $table field to make this class work
 
-    protected $allowedFields = ['event_id', 'url', 'logo', 'logo_mime_type', 'name', 'alt_text', 'copyright', 'visible_from'];
+    protected $allowedFields = ['event_id', 'url', 'logo', 'logo_mime_type', 'logo_alternative', 'logo_alternative_mime_type', 'name', 'alt_text', 'copyright', 'visible_from'];
     protected $useTimestamps = true;
 
     public function getPublished(int $eventId): array {
         return $this->db->table($this->table)
-            ->select('url, logo, name, copyright, alt_text')
+            ->select('url, logo, logo_mime_type, logo_alternative, logo_alternative_mime_type, name, copyright, alt_text')
             ->where('event_id', $eventId)
             ->where('visible_from <=', date('Y-m-d H:i:s'))
             ->get()
